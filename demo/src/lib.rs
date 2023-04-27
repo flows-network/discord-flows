84ac148babaf35c2c16feed42782b15519ad0fe2
Jinser <cmdr.jv@gmail.com>
2023-04-27 14:11:51 +0800

    Update demo

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,6 +1,25 @@
+use discord_flows::{get_client, listen_to_event, model::Message};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+
+    listen_to_event(token.clone(), move |msg| handle(msg, token)).await;
+
     println!("ok");
+}
+
+async fn handle(msg: Message, token: String) {
+    let client = get_client(token);
+    let channel_id = msg.channel_id;
+    let content = msg.content;
+    _ = client
+        .send_message(
+            channel_id.into(),
+            &serde_json::json!({
+                "content": format!("You said: {content}"),
+            }),
+        )
+        .await;
 }
